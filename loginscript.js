605617988cdad76affc53faef58afbf8249de563
Veysel <veyselbilici2000@gmail.com>
2023-12-19 00:44:12 +0300

    düzenleme

--- loginscript.js
+++ loginscript.js
@@ -4,16 +4,32 @@ $(document).ready(function () {
     $("#loginForm").submit(function (e) {
         e.preventDefault(); // Formun otomatik gönderilmesini engelle
 
-        // Kullanıcı adı ve şifre kontrolü (örnek kontrol, gerçek bir oturum kontrolü eklenmeli)
+        // Kullanıcı adı, şifre ve kullanıcı türü kontrolü (örnek kontrol, gerçek bir oturum kontrolü eklenmeli)
         var username = $("#username").val();
         var password = $("#password").val();
+        var userType = $("#userType").val();
 
-        if (username === "veysel" && password === "1234") {
-            // Başarılı giriş, ana sayfaya yönlendir
-            window.location.href = "index.html";
+        if (userType === "customer") {
+            // Müşteri girişi
+            if (username === "musteri" && password === "1234") {
+                // Başarılı giriş, ana sayfaya yönlendir
+                window.location.href = "index.html";
+            } else {
+                // Başarısız giriş, hata bildirimi göster
+                $("#loginError").show();
+            }
+        } else if (userType === "company") {
+            // Firma girişi
+            if (username === "firma" && password === "1234") {
+                // Başarılı giriş, firma sayfasına yönlendir
+                window.location.href = "index.html";
+            } else {
+                // Başarısız giriş, hata bildirimi göster
+                $("#loginError").show();
+            }
         } else {
-            // Başarısız giriş, hata bildirimi göster
-            $("#loginError").show();
+            // Geçerli bir kullanıcı türü seçilmediyse hata bildirimi göster
+            $("#loginError").show().text("Lütfen geçerli bir kullanıcı türü seçin.");
         }
     });
 });
