9fd0f31170b5ef6be5eca939f304955e9da1bac2
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-01-01 18:03:39 +0300

    Sirket logini eklendi

--- login/sirket-login.js
+++ login/sirket-login.js
@@ -1,21 +1,29 @@
-// loginscript.js
 $(document).ready(function () {
-    // Giriş formunu dinle
     $("#loginForm").submit(function (e) {
-        e.preventDefault(); // Formun otomatik gönderilmesini engelle
-
-        // Kullanıcı adı ve şifre kontrolü (örnek kontrol, gerçek bir oturum kontrolü eklenmeli)
-        var username = $("#username").val();
-        var password = $("#password").val();
-
-        if (username === "vestel" && password === "1234") {
-            // Başarılı giriş, ana sayfaya yönlendir
-            window.location.href = "sayfalar/sikayetlerim.html";
-            localStorage.setItem('username', username);
-
-        } else {
-            // Başarısız giriş, hata bildirimi göster
+      e.preventDefault();
+  
+      var username = $("#username").val();
+      var password = $("#password").val();
+  
+      $.ajax({
+        url: "http://localhost:3000/sirket",
+        type: "GET",
+        success: function (data) {
+          var sirketler = data;
+          var sirket = sirketler.find(function (sirket) {
+            return sirket.kullaniciAdi === username && sirket.sifre === password;
+          });
+  
+          if (sirket) {
+            window.location.href = "/sayfalar/sirket/sikayetlerim.html";
+            localStorage.setItem("kullaniciId", sirket.id);
+          } else {
             $("#loginError").show();
-        }
+          }
+        },
+        error: function (error) {
+          console.log("Error: ", error);
+        },
+      });
     });
-});
+  });
